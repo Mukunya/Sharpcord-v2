98d0e4ad16602dd1b68ec4e25be56cc4778c5769
Bán Vince <mukunya054@gmail.com>
2023-08-15 20:38:07 +0200

    Move out attributes, bot classes

---
+++ SharpcordBotLibrary/Attributes/DiscordBotAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpcord_bot_library
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class DiscordBotAttribute : Attribute
+    {
+        public enum BotType { gateway = 0, webhook = 1 };
+        public string DisplayName { get; set; }
+        public BotType Type { get; set; }
+        public DiscordBotAttribute(string displayName, BotType type)
+        {
+            DisplayName = displayName;
+            Type=type;
+        }
+
+    }
+}
---
+++ SharpcordBotLibrary/Modules/WebhookBot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpcord_bot_library
+{
+    public abstract class WebhookBot : Bot
+    {
+        internal static byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+        public WebhookServer server;
+        protected void InitBot(string endpoint, string publickey)
+        {
+            server = new WebhookServer(
+                endpoint,
+                NSec.Cryptography.PublicKey.Import(
+                    NSec.Cryptography.SignatureAlgorithm.Ed25519,
+                    StringToByteArray(publickey),
+                    NSec.Cryptography.KeyBlobFormat.RawPublicKey));
+            server.Interaction_create = c =>
+            {
+                Interaction(c);
+            };
+        }
+        abstract public void Interaction(Interaction i);
+        abstract public string GetBotToken();
+        override abstract public void Shutdown();
+    }
+}
